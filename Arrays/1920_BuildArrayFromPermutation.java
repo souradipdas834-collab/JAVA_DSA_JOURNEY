a1d537dc035596c38b9ac3cb8bef29ac464381ee
Souradip Das <souradipdas834@gmail.com>
2026-08-31 20:57:22 +0530

    Implement optimized buildArray method
    Added an optimized approach for building the array with O(1) auxiliary space.

--- Arrays/1920_BuildArrayFromPermutation.java
+++ Arrays/1920_BuildArrayFromPermutation.java
@@ -7,3 +7,19 @@ class Solution{
         return ans;
     }
 }
+// OPTIMIZED APPROACH : Math optimization to 0(1) Auxiliary Space
+
+// class Solution {
+//     public int[] buildArray(int[] nums) {
+//         int n=nums.length;
+//         for(int i=0;i< nums.length;i++){
+//             int targetIndex=nums[i]%n;
+//             int targetValue=nums[targetIndex]%n;
+//             nums[i]=nums[i]+(targetValue*n);
+//         }
+//         for(int i=0;i< nums.length; i++){
+//             nums[i]=nums[i]/n;
+//         }
+//         return(nums);
+//     }
+// }
