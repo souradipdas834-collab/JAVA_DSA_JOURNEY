4db49ae82faeff9c34c85b4e7437612d6db6f9d2
Souradip Das <souradipdas834@gmail.com>
2026-09-05 22:57:37 +0530

    Add optimized running sum solution
    Added an optimized solution for calculating the running sum using O(N) time and O(1) space.

--- Arrays/1480_RunningSumOf1DArray.java
+++ Arrays/1480_RunningSumOf1DArray.java
@@ -9,3 +9,12 @@ class Solution{
         return ans;
     }
 }
+// OPTIMISED SOLUTION USING O(N) TIME AND O(1) SPACE
+/* public int[] runningSum(int[] nums){
+        for(int i=1;i<nums.length; i++){
+            nums[i]+=nums[i-1];
+        }
+        return nums;
+    }
+}
+*/
