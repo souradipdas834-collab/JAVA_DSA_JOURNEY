6fcdd1b9cd658c036cd67ac0d5a759879ffa52ff
Souradip Das <souradipdas834@gmail.com>
2026-09-16 21:03:57 +0530

    Update 1512_NumberOfGoodPairs.java

--- Arrays/1512_NumberOfGoodPairs.java
+++ Arrays/1512_NumberOfGoodPairs.java
@@ -1,3 +1,4 @@
+// APPROACH 1: TIME COMPLEXITY: O(n^2) , SPACE COMPLEXITY: O(1)
 class Solution{
     public int numIdenticalPairs(int[] nums){
         int count=0;
@@ -12,3 +13,19 @@ class Solution{
         return count;
     }
 }
+
+// APPROACH 2: TIME COMPLEXITY: O(n) , SPACE COMPLEXITY: O(1)
+class Solution{
+    public int numIdenticalPairs(int[] nums){
+        int totalPairs = 0;
+        int[] counts = new int[101];
+        for(int i=0;i<nums.length;i++){
+            int currentNumber = nums[i];
+            totalPairs+ = counts[currentNumber];
+            counts[currentNumber]++;
+        }
+        return totalPairs;
+    }
+}
+            
+        
